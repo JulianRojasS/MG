19b9fc5d4c5cc08cebc5849f6c4a2e7c379345c2
julianrojass <julirojastoque@gmail.com>
2023-11-20 16:51:38 -0500

    v0.0.2

--- src/components/Carrusel/CarruselSlide.tsx
+++ src/components/Carrusel/CarruselSlide.tsx
@@ -1,6 +1,24 @@
+'use client'
+import styles from '@/styles/page.module.css'
+import { Key } from 'react'
+import { useState } from 'react'
+
 export default function CarruselSlide({prop}:{prop:any}) {
+    const [slide, SetSlide] = useState(0)
+    var style = styles.carruselCard
+    show(slide)
+    function show (slide: number) {
+        if (prop.id == slide) {
+            style = styles.carruselCardBlock  
+        }
+    }
+
+    function handdleClick () {
+        show()
+    }
+
     return (
-        <div id={prop.id}>
+        <div onClick={handdleClick} className={style} id={prop.id}>
             <img src={prop.src} alt={prop.alt}/>
         </div>
     )
--- src/components/Prom.tsx
+++ src/components/Prom.tsx
@@ -3,13 +3,14 @@ import styles from "@/styles/page.module.css"
 
 export default function Promo() {
     var data = [{src: "/modulares-logo.png", alt: "Image1", id: "0"}, {src: "/modulares-logo.png", alt: "Image2", id: "1"}, {src: "/modulares-logo.png", alt: "Image3", id: "2"}]
+    var currentSlide = 0
     return (
         <section>
             <h2>Promosiones de la semana</h2>
-            <div className={styles.carruselContainer}>
+            <div key={1} className={styles.carruselContainer}>
                 {data.map((e) => {
                     return (    
-                        <CarruselSlide prop={{src: e.src, alt: e.alt, id: e.id}}/>
+                            <CarruselSlide prop={{src: e.src, alt: e.alt, id: e.id, currentSlide}} key={e.id}/>
                     )
                 })}
             </div>
